bb73cc32032f60c48e879799e1cc2435acaeafb5
BalwantSandhu <bsingh3@unb.ca>
2026-07-15 15:26:15 -0300

    added create account component and styling

--- src/app/features/accounts/accounts.module.ts
+++ src/app/features/accounts/accounts.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ReactiveFormsModule } from '@angular/forms'; 
 import { AccountsRoutingModule } from './accounts-routing.module';
 import { CreateAccountComponent } from './create-account/create-account.component';
+import { SharedModule } from '../../shared/shared.module';
 
 
 @NgModule({
@@ -11,7 +12,9 @@ import { CreateAccountComponent } from './create-account/create-account.componen
   ],
   imports: [
     CommonModule,
-    AccountsRoutingModule
+    AccountsRoutingModule,
+    ReactiveFormsModule,
+    SharedModule
   ]
 })
 export class AccountsModule { }
--- src/app/features/accounts/create-account/create-account.component.ts
+++ src/app/features/accounts/create-account/create-account.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AccountService } from '../../../core/services/account.service';
+import { Router } from '@angular/router';
+import { AccountType } from '../../../core/models/account.model';
 
 @Component({
   selector: 'app-create-account',
@@ -7,5 +11,51 @@ import { Component } from '@angular/core';
   styleUrl: './create-account.component.scss'
 })
 export class CreateAccountComponent {
+  accountForm: FormGroup;
+  submitted = false;
 
+  constructor(
+    private fb: FormBuilder,
+    private accountService: AccountService,
+    private router : Router
+  ){
+    this.accountForm = this.fb.group({
+      name: ['', 
+        [
+          Validators.required,
+          Validators.minLength(2), Validators.maxLength(30)
+        ]
+      ],
+      type: ['chequing' as AccountType, Validators.required],
+      initialBalance: [0, 
+        [
+          Validators.required,
+          Validators.min(0)
+        ]
+      ]
+    });
+  }
+
+  get name() { return this.accountForm.get('name'); }
+  get type() { return this.accountForm.get('type'); }
+  get initialBalance() { return this.accountForm.get('initialBalance'); }
+
+  get selectedType(): AccountType{
+    return this.accountForm.get('type')?.value;
+  }
+
+  onSubmit(): void {
+
+    this.submitted = true;
+
+    if(this.accountForm.invalid) {
+      this.accountForm.markAllAsTouched();
+      return;
+    }
+
+    const { name, type, initialBalance } = this.accountForm.value;
+    const newAccount = this.accountService.createAccount(name, type, initialBalance);
+
+    this.router.navigate(['/dashboard']);
+  }
 }
