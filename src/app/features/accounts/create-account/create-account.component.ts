ebbc1b198fa2350e42d8df559b14c81f91ca42f5
BalwantSandhu <bsingh3@unb.ca>
2026-07-16 12:35:12 -0300

    Implemented the modal for create account

--- src/app/features/accounts/create-account/create-account.component.ts
+++ src/app/features/accounts/create-account/create-account.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { AccountService } from '../../../core/services/account.service';
 import { Router } from '@angular/router';
+import { AccountService } from '../../../core/services/account.service';
 import { AccountType } from '../../../core/models/account.model';
 
 @Component({
@@ -10,29 +10,23 @@ import { AccountType } from '../../../core/models/account.model';
   templateUrl: './create-account.component.html',
   styleUrl: './create-account.component.scss'
 })
-export class CreateAccountComponent {
+export class CreateAccountComponent implements OnDestroy {
   accountForm: FormGroup;
   submitted = false;
+
+  showModal = false;
+  modalCountdown = 3;
+  private countdownInterval: ReturnType<typeof setInterval> | null = null;
 
   constructor(
     private fb: FormBuilder,
     private accountService: AccountService,
-    private router : Router
-  ){
+    private router: Router
+  ) {
     this.accountForm = this.fb.group({
-      name: ['', 
-        [
-          Validators.required,
-          Validators.minLength(2), Validators.maxLength(30)
-        ]
-      ],
+      name: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(30)]],
       type: ['chequing' as AccountType, Validators.required],
-      initialBalance: [0, 
-        [
-          Validators.required,
-          Validators.min(0)
-        ]
-      ]
+      initialBalance: [0, [Validators.required, Validators.min(0)]]
     });
   }
 
@@ -40,22 +34,48 @@ export class CreateAccountComponent {
   get type() { return this.accountForm.get('type'); }
   get initialBalance() { return this.accountForm.get('initialBalance'); }
 
-  get selectedType(): AccountType{
+  get selectedType(): AccountType {
     return this.accountForm.get('type')?.value;
   }
 
   onSubmit(): void {
-
     this.submitted = true;
 
-    if(this.accountForm.invalid) {
+    if (this.accountForm.invalid) {
       this.accountForm.markAllAsTouched();
       return;
     }
 
     const { name, type, initialBalance } = this.accountForm.value;
-    const newAccount = this.accountService.createAccount(name, type, initialBalance);
+    this.accountService.createAccount(name, type, initialBalance);
 
+    this.startRedirectCountdown();
+  }
+
+  private startRedirectCountdown(): void {
+    this.showModal = true;
+    this.modalCountdown = 3;
+
+    this.countdownInterval = setInterval(() => {
+      this.modalCountdown--;
+      if (this.modalCountdown <= 0) {
+        this.goToDashboard();
+      }
+    }, 1000);
+  }
+
+  goToDashboard(): void {
+    if (this.countdownInterval) {
+      clearInterval(this.countdownInterval);
+      this.countdownInterval = null;
+    }
+    this.showModal = false;
     this.router.navigate(['/dashboard']);
+  }
+
+  ngOnDestroy(): void {
+    if (this.countdownInterval) {
+      clearInterval(this.countdownInterval);
+    }
   }
 }
